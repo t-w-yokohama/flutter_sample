ed2a2e528d26a1177eb8e2a2a1028f31d80ab7e3
t-w-yokohama <takero.watanabe.yokohama.gateway@gmail.com>
2021-02-03 22:54:27 +0900

    FlatButtonを複数個作成

--- lib/ui/screen/home_screen.dart
+++ lib/ui/screen/home_screen.dart
@@ -1,14 +1,89 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Hello World.',
-          style: TextStyle(fontWeight: FontWeight.bold),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ButtonTheme(
+              minWidth: 150.0,
+              child: FlatButton(
+                onPressed: () => print('Button 1'),
+                color: Colors.blue,
+                child: Text(
+                  'Button 1',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 30.0,
+            ),
+            ButtonTheme(
+              minWidth: 150.0,
+              child: FlatButton(
+                onPressed: () => print('Button 2'),
+                color: Colors.red,
+                child: Text(
+                  'Button 2',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 30.0,
+            ),
+            ButtonTheme(
+              minWidth: 150.0,
+              child: FlatButton(
+                onPressed: () => print('Button 3'),
+                color: Colors.yellow,
+                child: Text(
+                  'Button 2',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 30.0,
+            ),
+            ButtonTheme(
+              minWidth: 150.0,
+              child: FlatButton(
+                onPressed: () => print('Button 4'),
+                color: Colors.green,
+                child: Text(
+                  'Button 2',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 30.0,
+            ),
+            ButtonTheme(
+              minWidth: 150.0,
+              child: FlatButton(
+                onPressed: () => print('Button 5'),
+                color: Colors.orange,
+                child: Text(
+                  'Button 2',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 30.0,
+            ),
+          ],
         ),
       ),
     );
